eb87703e2ab23f595ef4eee888b5e20b12fbbb9b
yamasaki <takya45409025@icloud.com>
2025-01-22 15:46:58 +0900

    フッター作成

--- app/about.tsx
+++ app/about.tsx
@@ -1,4 +1,3 @@
-import { Footer } from "@/component/Footer/Page";
 import Image from "next/image";
 
 export default function Home() {
@@ -19,10 +18,11 @@ export default function Home() {
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
-            . 
+            .
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
+        
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
@@ -48,12 +48,55 @@ export default function Home() {
           >
             Read our docs
           </a>
-
-          <Footer/>
-
         </div>
       </main>
-     
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          言語
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
   );
 }
